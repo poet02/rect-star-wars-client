22bcc937306e89be9f54f1d7d0949a1938522d58
Thato <pooet02@gmail.com>
2021-04-18 13:43:01 +0200

    simple action query and display people

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment }  from 'react';
 import './App.css';
+import { useQuery } from '@apollo/client';
+import * as getPeople  from './operations/queries/__generated__/StarWarsCharacters';//type definition
+import { GET_STAR_WARS_CHARACTERS } from './operations/queries/getStarWarsCharacters'; //query or action
 
+function Home() {
+ 
+  const { data, loading, error } = useQuery<getPeople.StarWarsCharacters, getPeople.StarWarsCharactersVariables>
+    (GET_STAR_WARS_CHARACTERS, {
+      variables: {
+        search: 'l',
+        page: 1
+      }
+    });
+  if (loading || !data?.getPeople?.results) return <div>loading...</div>;
+  return (
+
+        <Fragment>
+          {data.getPeople.results.map((p) => (
+            <p key={p?.name}> {p?.name}</p>
+          ))}
+        </Fragment>
+  );
+
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
