967d1b44b4a099d739702c4598489904d8fa9aa1
Thato <pooet02@gmail.com>
2021-04-20 11:37:33 +0200

    landing as component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,12 @@
-import React, { Fragment }  from 'react';
 import './App.css';
-import { useQuery } from '@apollo/client';
-import * as getPeople  from './operations/queries/__generated__/StarWarsCharacters';//type definition
-import { GET_STAR_WARS_CHARACTERS } from './operations/queries/getStarWarsCharacters'; //query or action
+import { Navbar } from "./components/Navbar";
+import { Landing } from "./components/Landing";
 
-function Home() {
- 
-  const { data, loading, error } = useQuery<getPeople.StarWarsCharacters, getPeople.StarWarsCharactersVariables>
-    (GET_STAR_WARS_CHARACTERS, {
-      variables: {
-        search: 'l',
-        page: 1
-      }
-    });
-  if (loading || !data?.getPeople?.results) return <div>loading...</div>;
-  return (
-
-        <Fragment>
-          {data.getPeople.results.map((p) => (
-            <p key={p?.name}> {p?.name}</p>
-          ))}
-        </Fragment>
-  );
-
-}
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Landing />
     </div>
   );
 }
